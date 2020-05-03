e036afa1f06f003a26e2c2d917a8ea11952138dc
Mayank Patel <mayankp@digiflux.io>
2020-05-03 20:48:00 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerContent } from './screens/DrawerContent';
+import MainTabScreen from './screens/MainTabScreen';
+
+// import Home from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ExploreScreen from './screens/ExploreScreen';
+import DetailsScreen from './screens/DetailsScreen';
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        <Drawer.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Drawer.Screen name="ExploreScreen" component={ExploreScreen} />
+        <Drawer.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
+export default App
